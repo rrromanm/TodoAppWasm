cae70e5e4efd0c251bafaf2479c0c6171f548474
Romans Mihalonoks <roma.mihalonok@gmail.com>
2024-10-25 10:08:07 +0200

    Initial commit

---
+++ Shared/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared;
+
+public class Todo
+{
+    public int Id {get; set; }
+    public User Owner {get; set; }
+    public string Title {get; set; }
+    public bool IsCompleted {get; set; } = false;
+
+    public Todo(User user, string title)
+    {
+        Owner = user;
+        Title = title;
+    }
+}
---
+++ Shared/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared;
+
+public class User
+{
+    public List<Todo> Todos { get; set; }
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
