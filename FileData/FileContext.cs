e52284ccfa8424dfba042895240ec8c1926b2602
Romans Mihalonoks <roma.mihalonok@gmail.com>
2024-10-27 01:21:16 +0200

    setup done
    done until step 10

---
+++ FileData/FileContext.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using Shared;
+
+namespace FileData;
+
+public class FileContext
+{
+    private const string FilePath = "data.json";
+
+    private DataContainer? DataContainer;
+
+    public ICollection<Todo> Todos
+    {
+        get
+        {
+            LazyLoadData();
+            return DataContainer!.Todos;
+        }
+    }
+
+    public ICollection<User> Users
+    {
+        get
+        {
+            LazyLoadData();
+            return DataContainer!.Users;
+        }
+    }
+
+    private void LazyLoadData()
+    {
+        if (DataContainer == null)
+        {
+            LoadData();
+        }
+    }
+
+    private void LoadData()
+    {
+        string content = File.ReadAllText(FilePath);
+        DataContainer = JsonSerializer.Deserialize<DataContainer>(content);
+    }
+
+    public void SaveChanges()
+    {
+        string serialized = JsonSerializer.Serialize(DataContainer);
+        File.WriteAllText(FilePath, serialized);
+        DataContainer = null;
+    }
+}
