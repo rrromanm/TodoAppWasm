a4e809cb0f43288c7f27bb6e2d6168803c48de4c
Romans Mihalonoks <roma.mihalonok@gmail.com>
2024-10-31 10:18:33 +0100

    blazor new features

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -6,4 +6,9 @@ namespace HttpClients.ClientInterfaces;
 public interface ITodoService
 {
     Task CreateAsync(TodoCreationDTO dto);
+    Task<ICollection<Todo> > GetTodosAsync(string? username, int? userId, bool? isCompleted, string? titleContains);
+    Task UpdateAsync (TodoUpdateDTO dto);
+    Task<TodoBasicDTO> GetByIdAsync(int id);
+    
+    
 }
--- HttpClients/Implementations/TodoHttpClient.cs
+++ HttpClients/Implementations/TodoHttpClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using HttpClients.ClientInterfaces;
 using Shared;
@@ -23,5 +24,82 @@ public class TodoHttpClient : ITodoService
             string content = await response.Content.ReadAsStringAsync();
             throw new Exception(content);
         }
+    }
+
+    public async Task<ICollection<Todo>> GetTodosAsync(string? username, int? userId, bool? isCompleted,
+        string? titleContains)
+    {
+        string query = ConstructQuery(username, userId, isCompleted, titleContains);
+        HttpResponseMessage response = await client.GetAsync("https://localhost:7090/Todo" + query);
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+        
+        ICollection<Todo> todos = JsonSerializer.Deserialize<ICollection<Todo>>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return todos;
+    }
+
+    public async Task UpdateAsync(TodoUpdateDTO dto)
+    {
+        string dtoAsJson = JsonSerializer.Serialize(dto);
+        StringContent body = new StringContent(dtoAsJson, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PatchAsync("https://localhost:7090/Todo", body);
+        if (!response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+
+    public async Task<TodoBasicDTO> GetByIdAsync(int id)
+    {
+        HttpResponseMessage response = await client.GetAsync("https://localhost:7090/Todo/" + id);
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+        
+        TodoBasicDTO todo = JsonSerializer.Deserialize<TodoBasicDTO>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return todo;
+    }
+
+    private string ConstructQuery(string? username, int? userId,
+        bool? isCompleted, string? titleContains)
+    {
+        string query = "";
+        if (!string.IsNullOrEmpty(username))
+        {
+            query += $"?username={username}";
+        }
+
+        if (userId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"userid={userId}";
+        }
+
+        if (isCompleted != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"completedstatus={isCompleted}";
+        }
+
+        if (!string.IsNullOrEmpty(titleContains))
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"titlecontains={titleContains}";
+        }
+
+        return query;
     }
 }
